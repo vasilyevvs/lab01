67bc7edc75ebb6abe1d3a7459937355eacadea0d
666vasilyev <Danya2002>
2021-10-10 18:22:19 +0300

    first look

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,11 +1,15 @@
+#include <nlohmann/json.hpp>
+#include <sstream>
+
+#include "any"
+#include "fstream"
+#include "header.hpp"
+#include "iomanip"
 #include "iostream"
 #include "string"
-#include "iomanip"
-#include "any"
-#include "header.hpp"
 using namespace std;
-using nlohmann::json
-    string filePath;
+using nlohmann::json;
+string filePath;
 
 struct student_t {
   string name;
@@ -13,12 +17,6 @@ struct student_t {
   any avg;
   any debt;
 };
-void from_json(const json& j, student_t& s) {
-  s.name = get_name(j.at("name"));
-  s.group = get_group(j.at("group"));
-  s.avg = get_avg(j.at("avg"));
-  s.debt = get_debt(j.at("debt"));
-}
 
 auto get_name(const json& j) -> string {
   return j.get<string>();
@@ -46,41 +44,33 @@ auto get_avg(const json& j) -> any {
 
 auto get_group(const json& j) -> any {
   if (j.is_string())
-    return = j.get<string>();
+    return j.get<string>();
   else
     return j.get<size_t>();
 }
 
-string stringi(any& item)
-{
-  stringstream ss;
-  if(item.type() == typeid(nullptr_t))
-    ss << "null";
-  else if(item.type() == typeid(string))
-    ss << any_cast<string>(item);
-  else if(item.type() == typeid(int))
-    ss << any_cast<int>(item);
-  else if(item.type() == typeid(float))
-    ss << any_cast<float>(item);
-
+void from_json(const json& j, student_t& s) {
+  s.name = get_name(j.at("name"));
+  s.group = get_group(j.at("group"));
+  s.avg = get_avg(j.at("avg"));
+  s.debt = get_debt(j.at("debt"));
 }
 
-int main() {
-  ifstream("../1.json");
-  if (!file) {
-    throw std::runtime_error{"unable to open json: " + jsonPath};
-  }
-
-  json data;
-  file >> data;
-
-  vector<student_t> students;
-  for (auto const& item : data.at("items")) {
-    student_t s;
-    from_json(item, s);
-    students.push_back(student);
-  }
-  print(students, std::cout);
+std::string toString(std::any& item)
+{
+  std::stringstream ss;
+  if (item.type() == typeid(std::nullptr_t))
+    ss << "null";
+  else if (item.type() == typeid(std::string))
+    ss << std::any_cast<std::string>(item);
+  else if (item.type() == typeid(double))
+    ss << std::any_cast<double>(item);
+  else if (item.type() == typeid(std::vector<std::string>))
+    ss << std::any_cast<std::vector<std::string> >(item).size();
+  else if (item.type() == typeid(size_t))
+    ss << std::any_cast<size_t>(item);
+  else  ss << "unknown";
+  return ss.str();
 }
 
 void print(string s1, string s2, string s3, string s4, std::ostream& os)
@@ -93,11 +83,30 @@ void print(string s1, string s2, string s3, string s4, std::ostream& os)
 }
 void print(student_t& student, std::ostream& os)
 {
-  print(student.name, student.group, student.avg,  student.debt, os);
+  print(student.name, toString(student.group), toString(student.avg),  toString(student.debt), os);
 }
-void print(vector<student_t>& students, std::ostream& os)
-{
-  print( "name", "group", "avg", "debt", os);
+void print(vector<student_t>& students, std::ostream& os) {
+  print("name", "group", "avg", "debt", os);
   for (student_t& student : students) {
     print(student, os);
+  }
+}
+  int main()
+  {
+    string jsonPath("../1.json");
+    ifstream file(jsonPath);
+    if (!file) {
+      throw std::runtime_error{"unable to open json: " + jsonPath};
+    }
+
+    json data;
+    file >> data;
+
+    vector<student_t> students;
+    for (auto const& item : data.at("items")) {
+      student_t s;
+      from_json(item, s);
+      students.push_back(s);
+    }
+    print(students, std::cout);
   }
